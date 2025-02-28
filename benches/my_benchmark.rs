8917c7bc6444d2e785f141373857dabaf9bef4c8
Lenny0939 <lennyescott@gmail.com>
2025-03-01 08:29:41 +1100

    start of benches

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,16 +1,59 @@
 use chogalyzer::*;
+use chogalyzer::generation::attempt_swap;
+use std::mem::swap;
+use std::env::args;
+use std::fs;
 use diol::prelude::*;
 
 fn main() -> std::io::Result<()> {
     let mut bench = Bench::new(BenchConfig::from_args()?);
-    bench.register(slice_times_two, [4, 8, 16, 128, 1024]);
+    let layout_letters: String = fs::read_to_string("whirl.txt")
+        .expect("couldn't read layout")
+        .replace([' ', ' '], "")
+        .replace('_', "⎵")
+        .chars()
+        .collect();
+
+    // has to be 37 because ⎵ is a few extra bytes
+    let layout_raw: [char; 32] = layout_letters[..37]
+        .replace('\n', "")
+        .chars()
+        .collect::<Vec<char>>()
+        .try_into()
+        .expect("couldn't read layout");
+
+    let corpus: String = fs::read_to_string("mr.txt")
+        .expect("error reading corpus")
+        .to_lowercase()
+        .replace(['\n', ' '], "⎵")
+        .chars()
+        .filter(|ch| layout_raw.contains(ch))
+        .collect();
+
+    let magic_rules_raw = layout_letters[38..].split('\n');
+    let mut magic_rules: Vec<String> = Vec::default();
+
+    for rule in magic_rules_raw {
+        magic_rules.push(rule.to_string());
+    }
+
+    let stats = stats::analyze(
+        corpus.clone(),
+        layout_raw,
+        &"bench".to_string(),
+        magic_rules.clone(),
+    );
+
+    let mut ngram_vec: Vec<([char; 3], u32)> = stats.ngram_table.clone().into_iter().collect();
+    ngram_vec.sort_by(|a, b| b.1.cmp(&a.1));
+    bench.register(swap_letters, layout);
+    bench.register(swap_letters, (layout);
     bench.run()?;
     Ok(())
 }
-fn slice_times_two(bencher: Bencher, len: usize) {
-    let mut v = vec![0.0_f64; len];
-    bencher.bench(|| {
-        // stats::analyze();
-        black_box(&mut v);
-    });
+
+fn swap_letters(bencher: Bencher, corpus: &String) {
+    layout = "abcdefghijklmnopqrstuv,.';*⎵".shuffle();
+    old_stats = attempt_swap(do_magic, layout, magic, corpus, old_stats, bad_bigrams, temparature);
+    bencher.bench(|| attempt_swap(false, layout, Vec::default(), corpus, old_stats, bad_bigrams, temparature))
 }
